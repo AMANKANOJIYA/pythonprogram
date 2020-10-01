c74ce5eaf63d687f9633bbf7109b7328ccbf04f0
AMANKANOJIYA <50018596+AMANKANOJIYA@users.noreply.github.com>
2020-10-01 20:54:58 +0530

    Update and rename README.md to bmi_calculater.py

---
+++ bmi_calculater.py
@@ -0,0 +1,14 @@
+# pythonprogram
+name=input("enter your name:)
+height=int(input("enter your height:))
+weight=float(input("enter your weight:"))
+bmi=weight/(height**2)
+print(f"BMI : {bmi}")
+if bmi < 25 :
+    print(name)
+    print("is not overweight")
+else :
+    print(name)
+    print("the person is overweigth")
+    
+#Thank you sir You are the Best
